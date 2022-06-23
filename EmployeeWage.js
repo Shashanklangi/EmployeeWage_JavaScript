e3a4d05b6729b4cfb251e32f7fd2fc9f3762b465
Shashank <langishashank@gmail.com>
2022-06-23 17:06:31 +0530

    [Shashank]:Calculating Employee Wages with a Condition.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,22 +1,23 @@
 // UC1:Checking Employee Present or Absent
-const Is_Present = 1
-let EmpCheck = Math.floor(Math.random() * 10) % 2;
-if(EmpCheck == Is_Present)
-{
-    console.log("Employee is Present");
-}
-else
-{
-    console.log("Employee is Absent");
-}
+// const Is_Present = 1
+// let EmpCheck = Math.floor(Math.random() * 10) % 2;
+// if(EmpCheck == Is_Present)
+// {
+//     console.log("Employee is Present");
+// }
+// else
+// {
+//     console.log("Employee is Absent");
+// }
 
-// UC4:Checking Employee Wage For a Month
+// UC5:Checking Employee Wage with Condition
 const PartTime = 1;
 const FullTime = 2;
 const PartTimeHours = 4;
 const FullTimeHours = 8;
 const WageperHour = 20;
 const WorkingDays = 20;
+const MaxHours_In_A_Month = 160;
 
 function CalculateDailyWage(CheckEmp)
 {
@@ -31,10 +32,12 @@ function CalculateDailyWage(CheckEmp)
 }
 }
 let TotalEmpHrs = 0;
-for(let day = 0; day < WorkingDays; day++)
+let TotalWorkingDays = 0;
+while(TotalEmpHrs <= MaxHours_In_A_Month && TotalWorkingDays < WorkingDays)
 {
+    TotalWorkingDays++;
     let CheckEmp = Math.floor(Math.random() * 10) % 3;
     TotalEmpHrs += CalculateDailyWage(CheckEmp);
 }
 let EmpWage = TotalEmpHrs * WageperHour;
-console.log("Hours: " + TotalEmpHrs + "\nEmployee Wage is: " + EmpWage);
+console.log("Total Days: " + TotalWorkingDays + "\nTotal Hours: " + TotalEmpHrs +"\nEmployee Wage is: " + EmpWage);
