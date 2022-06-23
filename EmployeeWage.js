350b8b6505510ebf92a58c3696c1cbcd22cad0ce
Shashank <langishashank@gmail.com>
2022-06-23 17:08:14 +0530

    [Shashank]:Calculating Employee Wages with a Condition.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,14 +1,14 @@
 // UC1:Checking Employee Present or Absent
-// const Is_Present = 1
-// let EmpCheck = Math.floor(Math.random() * 10) % 2;
-// if(EmpCheck == Is_Present)
-// {
-//     console.log("Employee is Present");
-// }
-// else
-// {
-//     console.log("Employee is Absent");
-// }
+const Is_Present = 1
+let EmpCheck = Math.floor(Math.random() * 10) % 2;
+if(EmpCheck == Is_Present)
+{
+    console.log("Employee is Present");
+}
+else
+{
+    console.log("Employee is Absent");
+}
 
 // UC5:Checking Employee Wage with Condition
 const PartTime = 1;
