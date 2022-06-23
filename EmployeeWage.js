d65714e2d4ad7070b4fc63936c28608d945c0301
Shashank <langishashank@gmail.com>
2022-06-23 14:57:35 +0530

    [Shashank]:Checking Employee PartTime and FullTime.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,3 +1,4 @@
+// UC1:Checking Employee Present or Absent
 const Is_Present = 1
 let EmpCheck = Math.floor(Math.random() * 2);
 if(EmpCheck == Is_Present)
@@ -8,3 +9,26 @@ else
 {
     console.log("Employee is Absent");
 }
+
+// UC2:Checking Employee PartTime And FullTime
+const PartTime = 1;
+const FullTime = 2;
+const PartTimeHours = 4;
+const FullTimeHours = 8;
+const WageperHour = 20;
+let Emphours = 0;
+let CheckEmp = Math.floor(Math.random() * 3);
+
+switch(CheckEmp)
+{
+    case PartTime:
+        Emphours = PartTimeHours;
+        break;
+    case FullTime:
+        Emphours = FullTimeHours;
+        break;
+    default:
+        Emphours = 0;
+}
+let EmpWage = Emphours * WageperHour;
+console.log("Employee Wage is: " + EmpWage);
